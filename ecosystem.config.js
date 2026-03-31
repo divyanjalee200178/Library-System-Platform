33a9f8587dd6cc97126dd788a1e4cc41b8e4e5b5
divyanjalee200178 <induminisilva48@gmail.com>
2026-03-31 13:25:03 +0530

    chnage ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,27 @@ module.exports = {
     apps: [
         {
             name: "config-server",
-            args: "java -jar target ./config-server/config-server-1.0.0.jar",
-            log_files: "./logs/config-server.log",
+            script: "java",
+            args: "-jar ./config-server/target/config-server-1.0.0.jar",
+            out_file: "./logs/config-server.log",
+            error_file: "./logs/config-server.log",
+            merge_logs: true
         },
         {
             name: "Service-registry",
-            args: " java -jar ./Service-registry/target/service-registry-1.0.0.jar",
-            log_files: "./logs/service-registry.log",
+            script: "java",
+            args: "-jar ./Service-registry/target/service-registry-1.0.0.jar",
+            out_file: "./logs/service-registry.log",
+            error_file: "./logs/service-registry.log",
+            merge_logs: true
         },
         {
             name: "api-gateway",
-            args: "java -jar ./api-gateway/target/api-gateway-1.0.0.jar",
-            log_files: "./logs/api-gateway.log",
+            script: "java",
+            args: "-jar ./api-gateway/target/api-gateway-1.0.0.jar",
+            out_file: "./logs/api-gateway.log",
+            error_file: "./logs/api-gateway.log",
+            merge_logs: true
         }
     ]
 };
